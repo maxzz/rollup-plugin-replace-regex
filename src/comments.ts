9742488dac72acb3dfab6f97b27e2d5dba611622
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-28 18:16:16 -0700

    WIP. comments. convert to es6 module

--- src/comments.ts
+++ src/comments.ts
@@ -1,31 +1,35 @@
-class ConditionalComments {
-    static checkConditions: boolean = true;
+//class ConditionalComments {
+    const checkConditions: boolean = true;
 
-    private static isAllowed(name: string): boolean {
-        if (!this.checkConditions) {
+    const definedNames: Set<string> = new Set();
+    const re = /\/\*\s*(?:\[\s*(\w+\s*(?:,\s*?\w+)*)\s*\])??\s*({|}|{}|<>)\s*\*\//; // m1 - definition names (separated by commas); m2 - operation: '{}', '//', '{', '}', '<>'
+    const filler = { '{}': '//', '{': '/*', '}': '*/', '<>': '' };
+
+    function isAllowed(name: string): boolean {
+        if (!checkConditions) {
             return false;
         }
         if (!name) {
             return true;
         }
-        let rv = name.split(',').map((s: string) => s.trim()).every((s: string) => this.definedNames.has(s));
+        let rv = name.split(',').map((s: string) => s.trim()).every((s: string) => definedNames.has(s));
         return rv;
     }
 
-    private static whitespace = /^(\s+)/;
-    private static caclIndent(lines: string[], from_: number, to_: number): string {
+    const whitespace = /^(\s+)/;
+    function caclIndent(lines: string[], from_: number, to_: number): string {
         let rv = 0;
         for (let i = from_; i <= to_; i++) {
-            let space = this.whitespace.exec(lines[i]);
+            let space = whitespace.exec(lines[i]);
             if (space && (space[1].length < rv || i === from_)) {
                 rv = space[1].length;
             }
         }
         return (' ' as any).repeat(rv);
-    } //commentLines()
+    }
 
-    private static commentLines(lines: string[], from_: number, to_: number): void {
-        let indent = this.caclIndent(lines, from_, to_);
+    function commentLines(lines: string[], from_: number, to_: number): void {
+        let indent = caclIndent(lines, from_, to_);
 
         for (let i = from_; i <= to_; i++) {
             if (lines[i].length > indent.length) {
@@ -34,13 +38,9 @@ class ConditionalComments {
                 lines[i] = `${indent}//${lines[i]}`;
             }
         }
-    } //commentLines()
+    }
 
-    private static definedNames: Set<string> = new Set();
-    private static re = /\/\*\s*(?:\[\s*(\w+\s*(?:,\s*?\w+)*)\s*\])??\s*({|}|{}|<>)\s*\*\//; // m1 - definition names (separated by commas); m2 - operation: '{}', '//', '{', '}', '<>'
-    private static filler = { '{}': '//', '{': '/*', '}': '*/', '<>': '' };
-
-    private static matchAll(re: string, s: string): string[][] {
+    function matchAll(re: string, s: string): string[][] {
         // <-
         // "        /*[traceDc]{}*/ /*[csRawMutations]{}*/ this.muTrace && this.muTrace.traceMutations(muts);"
         // ->
@@ -57,11 +57,11 @@ class ConditionalComments {
         return rv;
     }
 
-    static printReport(report: string[]) {
+    export function printReport(report: string[]) {
         report.forEach(_ => console.log(_));
     }
 
-    static commentFile(cnt: string): string {
+    export function commentFile(cnt: string): string {
         let lines: string[] = cnt.split(/\r??\n/g); // or let lines = content.split(/\r?\n/g); // The best without empty lines: cnt.split(/[\r\n]+/g); // Preserve line numbers: /\r??\n/g
 
         let beginBlockIdx: number = -1;
@@ -70,17 +70,17 @@ class ConditionalComments {
         let report: string[] = [];
 
         lines.forEach((line: string, index: number) => {
-            let m = line.match(this.re);
+            let m = line.match(re);
             if (m) {
                 if (m[2] === '<>') {
-                    this.definedNames.add(m[1]);
+                    definedNames.add(m[1]);
                     return;
                 }
 
                 if (m[2] === '{}') {
-                    let all = this.matchAll(this.re.source, line);
+                    let all = matchAll(re.source, line);
                     let allow = all.every((match: string[]) => {
-                        return this.isAllowed(match[1])
+                        return isAllowed(match[1])
                     });
                     if (!allow) {
                         lines[index] = lines[index].replace(/^(\s+)(.*)/, (s, p1, p2) => `${p1}// ${p2}`); // block is not allowed. replace with whitespace, '//', and the rest.
@@ -94,7 +94,7 @@ class ConditionalComments {
                     blockNesting++;
 
                     if (beginBlockIdx < 0) { // If we are not inside block
-                        if (!this.isAllowed(m[1])) {
+                        if (!isAllowed(m[1])) {
                             beginBlockIdx = index;
                         }
                     }
@@ -104,7 +104,7 @@ class ConditionalComments {
                 if (m[2] === '}') {
                     blockNesting--;
                     if (blockNesting < 0) {
-                        this.printReport(report);
+                        printReport(report);
                         throw new Error(`TM: mismatched pre-processor comment blocks '}'. missing prev open comment block.`)
                     }
 
@@ -112,7 +112,7 @@ class ConditionalComments {
 
                     if (blockNesting === 0) {
                         if (beginBlockIdx >= 0) { // If we are inside block
-                            this.commentLines(lines, beginBlockIdx, index);
+                            commentLines(lines, beginBlockIdx, index);
                             beginBlockIdx = -1;
                         }
                     }
@@ -121,12 +121,12 @@ class ConditionalComments {
         });
 
         if (blockNesting !== 0) {
-            this.printReport(report);
+            printReport(report);
             throw new Error(`TM: mismatched pre-processor comment blocks {!==0}. missing prev closing comment block.`)
         }
 
         return lines.join('\r\n');
     } //commentFile()
 
-} //class ConditionalComments
+//} //class ConditionalComments
 
